608c22c4ff157aa2ebfbe7b2c0bd2c481a381cbe
WineJay <hossaina3745@masseyhigh.school.nz>
2026-03-05 10:21:02 +1300

    Gets colours, score not yet working

---
+++ C_03_get_all_colours.py
@@ -0,0 +1,30 @@
+import csv
+import random
+
+#retrieve colours from csv file and put them in a list
+file= open("00_colour_list_hex_v3.csv", "r")
+all_colours = list(csv.reader(file, delimiter=","))
+file.close()
+
+
+# remove first row
+all_colours.pop(0)
+
+round_colours = []
+colour_scores = []
+
+# loop until we have four colours with different scores....
+while len(round_colours) < 4:
+    potential_colour = random.choice(all_colours)
+
+    # get scores and check it's not a duplicate
+    if potential_colour[1] not in colour_scores:
+        round_colours.append(potential_colour)
+        colour_scores.append(potential_colour[1])
+
+print(round_colours)
+print(colour_scores)
+
+# find target score (median)
+
+#
